2927daa0bbc5ac0a72430237827ad320f4b4a65d
aleksey-10 <alexcare1337@gmail.com>
2020-02-29 21:55:47 +0200

    Updates

---
+++ precache-manifest.a49abf3630f726b648d9a1dde658f6d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd6e8f86bd19a8466dcfc22a714ecad",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "44fcbce4de1327ec62c5",
+    "url": "/snake/static/css/main.702a1b02.chunk.css"
+  },
+  {
+    "revision": "92651d3f8f9ff1c05c44",
+    "url": "/snake/static/js/2.5b09c965.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/snake/static/js/2.5b09c965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fcbce4de1327ec62c5",
+    "url": "/snake/static/js/main.18c4b20b.chunk.js"
+  },
+  {
+    "revision": "c4ed2f3e17ca80d81af6",
+    "url": "/snake/static/js/runtime-main.5c3cbe91.js"
+  },
+  {
+    "revision": "03526336ef23211c5a4c91db0ff12d17",
+    "url": "/snake/static/media/apple.03526336.png"
+  }
+]);
